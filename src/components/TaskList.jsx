058e93a69fd49223349a6dcc12fd98e40aebc1db
Daniel Su <foeverdan@gmail.com>
2024-04-09 14:30:00 +1000

    Add TaskLsit

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,19 @@
+import Task from './Task';
+
+export default function TaskLsit ({loading, tasks, onPinTask, onArchiveTask}) {
+    const events = {
+        onPinTask,
+        onArchiveTask,
+    }
+    if(loading){return <div className='list-items'>loading</div>}
+    if(tasks.length === 0 ){return <div className='list-items'>empty</div>}
+    return (
+        <div className='list-items'>
+            {tasks.map(task=>{
+                return (
+                    <Task key={task.id} task={task} {...events}/>
+                )
+            })}
+        </div>
+    )
+}
